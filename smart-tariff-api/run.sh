a6ef592d52a4cb83024da2eeee2b844a56f9c620
PlutoniumOD <gavincliffe42@outlook.com>
2026-03-17 16:48:34 +0000

    Update run.sh

--- smart-tariff-api/run.sh
+++ smart-tariff-api/run.sh
@@ -10,5 +10,9 @@ fi
 # Make options path visible to the app
 export ADDON_OPTIONS_PATH="/data/options.json"
 
+# Ensure we're in /app so Python can import the "app" package
+cd /app
+export PYTHONPATH=/app
+
 # Use the virtualenv we created in the Dockerfile
-exec /app/venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8787
+exec /app/venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8787 --log-level info
